d656721c55477be2b07f0fe215ecd03d113cfcce
yeppiee <pakhotnykh.i@gmail.com>
2025-03-23 23:12:50 +0300

    feat: c

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 
 import './App.css';
+
+
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+
+
 
 
 
@@ -17,7 +21,7 @@ function App() {
   return (
     <>
       <div>
-            <a href="https://vite.dev" target="_blank">
+                  <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
