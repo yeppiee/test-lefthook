47a3d9a755a0daafc5b85588e70a5ccab0c96863
yeppiee <pakhotnykh.i@gmail.com>
2025-03-23 21:37:45 +0300

    feat: c

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
 
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+
+
+
+import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +13,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+            <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
