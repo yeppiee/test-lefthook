2d8640ccf6cfd03e71f119f2fea51553202dc3e3
yeppiee <pakhotnykh.i@gmail.com>
2025-06-12 19:37:25 +0300

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import { useState } from 'react';
-
-
 
 import './App.css';
 import reactLogo from './assets/react.svg';
