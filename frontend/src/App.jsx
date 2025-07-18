70bdb33ab6f9b03d8c23ad6d54c3d336ccb53332
yeppiee <pakhotnykh.i@gmail.com>
2025-07-18 16:58:28 +0300

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className="main">
+    <main>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
